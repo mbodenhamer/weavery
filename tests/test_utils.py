7ca2f2798509b7ccd67251e8d543f0848e2a2ba9
mbodenhamer <mbodenhamer@mbodenhamer.com>
2017-10-03 17:18:50 -0500

    Adding tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,20 @@
-from weavery import groups
+from fabric.tasks import execute
+from fabric.state import env
+from fabric.api import settings
+from dockerman import container
+from syn.base_utils import assign
 
-def test():
-    groups
+from weavery import groups, users, success, success_sudo
+
+#-------------------------------------------------------------------------------
+
+def test1():
+    with container('rastasheep/ubuntu-sshd', detach=True) as c:
+        with assign(env, 'host_string', c.status.ip_addr):
+            with assign(env, 'user', 'root'):
+                with assign(env, 'password', 'root'):
+                    assert success('true')
+                    with settings(warn_only=True):
+                        assert not success('false')
+
+#-------------------------------------------------------------------------------
